5e753536fef1b0cf58c6498e8da9fecf3917871d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 13:47:51 +0300

    Added a while statement to the code

--- Scripts/Hello.gd
+++ Scripts/Hello.gd
@@ -1,18 +1,10 @@
 extends Node
 
 func _init():
-	var choice:int = 1;
+	var x:int = 0;
 	
-	match choice: 
-		1:
-			print("You chose 1")
-		2:
-			print("You chose 2")
-		3:
-			print("Ypu chose 3")
-		_:
-			print("Default option")
-			
-	print("We are outside the match structure")
+	while(x < 10):
+		print("x"+str(x))
+		x+=1;
 
 # I am a comment. Don't mind me
