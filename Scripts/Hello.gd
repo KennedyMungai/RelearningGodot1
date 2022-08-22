d23f94de761e5b85e3f00b81f29d738e0e66775a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 12:56:17 +0300

    Added another faulty variable declaration

--- Scripts/Hello.gd
+++ Scripts/Hello.gd
@@ -1,9 +1,11 @@
 extends Node
 
 onready var life: int = 3
+onready var somebodys_name: String = "Patrick"
 
 func _init():
 	print("You know my name")
 	print(life)
+	print(somebodys_name)
 
 # I am a comment. Don't mind me
