dd01704812a35177bf95e7d96c1566b96b049113
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 15:14:31 +0300

    Tried with the default parameter values

--- Scripts/ScriptNoob.gd
+++ Scripts/ScriptNoob.gd
@@ -4,7 +4,8 @@ var number: int
 var noob_name: String
 
 func _ready():
-	var my_bike:Bike = Bike.new(10.0, "Gold", "Shark Helicopter");
+	#var my_bike:Bike = Bike.new(10.0, "Gold", "Shark Helicopter");
+	var my_bike:Bike = Bike.new();
 	my_bike.display_color();
 	my_bike.display_name();
 	my_bike.accelerate();
