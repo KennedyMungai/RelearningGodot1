ea7f7ab47a41f7f7e396bfd45663892663cfe644
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 14:27:38 +0300

    Defined and called a function with some parameters

--- Scripts/Hello.gd
+++ Scripts/Hello.gd
@@ -1,11 +1,10 @@
 extends Node
 
 func _init():
-	var x:int = 0;
-	var Bike = load("res://Scripts/bike.gd")
-	var my_bike = Bike.new()
-	
-	my_bike.accelerate()
-	Bike.say_hello()
+	var number:int = calculate_sum(1,2);
+	print(number);
 
-# I am a comment. Don't mind me
+func calculate_sum(a:int, b:int):
+	return(a+b);
+	
+	
