861dea5d1538bcc73410c7f01c735eb2947eb897
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 15:02:01 +0300

    Created a script for the bike class and gave it a basic constructor with initialized values

---
+++ Scripts/Bike.gd
@@ -0,0 +1,10 @@
+class_name Bike
+
+var speed: float
+var color: String
+var bike_name: String
+
+func _init(new_speed: float = 5.5, new_color: String = "Purple", new_bike_name: String = "Bike Yangu"):
+	speed = new_speed;
+	color = new_color;
+	bike_name = new_bike_name;
