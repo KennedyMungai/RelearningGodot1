b79f157218ec69b9dd19dae2c026eed8761d135f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 13:09:06 +0300

    Made some changes in the code

--- Scripts/Hello.gd
+++ Scripts/Hello.gd
@@ -2,11 +2,10 @@ extends Node
 
 var life: int = 3
 var somebodys_name: String = "Patrick"
-var names:Array = []
+var names:Array = ["Paul", "Mary", "John", "Mark", "Eva", "Pat", "Sinead", "Elma", "Flaithri", "Eleanor"]
 
 func _init():
-	print("You know my name")
-	print(life)
-	print(somebodys_name)
+	if(somebodys_name and life):
+		print("Hey ya!")
 
 # I am a comment. Don't mind me
