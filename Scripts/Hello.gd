a8a7ccb39ee822824e610ea1a0134943b4b44316
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 14:32:25 +0300

    Created a display full name function that returns a void and prints out the full name to the console

--- Scripts/Hello.gd
+++ Scripts/Hello.gd
@@ -3,8 +3,10 @@ extends Node
 func _init():
 	var number:int = calculate_sum(1,2);
 	print(number);
+	display_full_name("Barack", "Obama");
 
 func calculate_sum(a:int, b:int) -> int:
 	return(a+b);
 	
-	
+func display_full_name(f_name:String="Peter", l_name: String = "Griffin") -> void:
+	print(f_name+" "+l_name);
